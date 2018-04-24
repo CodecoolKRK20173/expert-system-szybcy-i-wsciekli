b1ec3c1548fec04e1095499982c6052e81b0fdc4
Maciej Sikora <m.j.p.sik@gmail.com>
2018-04-24 11:26:38 +0200

    Correct clean code consistency

--- src/main/java/com/codecool/expertsystem/questionnaire/Fact.java
+++ src/main/java/com/codecool/expertsystem/questionnaire/Fact.java
@@ -7,37 +7,31 @@ import java.util.Set;
 /**
  * Class representing fact.
  */
-public class Fact
-{
+public class Fact {
     private Map<String, boolean> valueMap;
     private final String description;
     private final String id;
     private boolean value;
 
-    public Fact(String id, String description)
-    {
+    public Fact(String id, String description) {
         this.id = id;
         this.valueMap = new HashMap<String, boolean>();
         this.description = description;
     }
 
-    public Set<String> getIdSet()
-    {
+    public Set<String> getIdSet() {
         return this.valueMap.keySet();
     }
 
-    public void setFactValueById(String id, boolean value)
-    {
+    public void setFactValueById(String id, boolean value) {
         this.valueMap.put(id, value);
     }
 
-    public String getDescription()
-    {
+    public String getDescription() {
         return this.description;
     }
 
-    public boolean getValueById(String id)
-    {
+    public boolean getValueById(String id) {
         return this.valueMap.get(id);
     }
 
