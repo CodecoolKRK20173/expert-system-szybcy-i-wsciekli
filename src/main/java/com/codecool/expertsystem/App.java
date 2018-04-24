37bd54e3f0d40e46154e7581e5ee5953f24d4784
rafalkordas <rafalkordas063@gmail.com>
2018-04-24 14:36:00 +0200

    Reverse changes in App.java

--- src/main/java/com/codecool/expertsystem/App.java
+++ src/main/java/com/codecool/expertsystem/App.java
@@ -1,17 +1,13 @@
 package com.codecool.expertsystem;
 
-import com.codecool.expertsystem.parsers.FactParser;
-import com.codecool.expertsystem.parsers.XMLParser;
-
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        XMLParser parser = new FactParser();
-        ((FactParser) parser).createFacts();
+        System.out.println( "Hello World!" );
     }
 }
