28fbd4807821d5eaee22a0d804dc006a5176c9ec
Maciej Sikora <m.j.p.sik@gmail.com>
2018-04-24 11:21:18 +0200

    Correct clean code consistency

--- src/main/java/com/codecool/expertsystem/questionnaire/Question.java
+++ src/main/java/com/codecool/expertsystem/questionnaire/Question.java
@@ -3,37 +3,31 @@ package com.codecool.expertsystem.questionnaire;
 /**
  * Class representing question.
  */
-public class Question
-{
+public class Question {
     private final String id;
     private final String question;
     private Answer answer;
 
-    public Question(String id, String question, Answer answer)
-    {
+    public Question(String id, String question, Answer answer) {
         this.id = id;
         this.answer = answer;
         this.question = question;
     }
 
 
-    public String getId()
-    {
+    public String getId() {
         return this.id;
     }
 
-    public String getQuestion()
-    {
+    public String getQuestion() {
         return this.question;
     }
 
-    public Answer getAnswer()
-    {
+    public Answer getAnswer() {
         return this.answer;
     }
 
-    public boolean getEvaluatedAnswer(String input)
-    {
+    public boolean getEvaluatedAnswer(String input) {
         return answer.evaluateAnswerByInput(input);
     }
 
