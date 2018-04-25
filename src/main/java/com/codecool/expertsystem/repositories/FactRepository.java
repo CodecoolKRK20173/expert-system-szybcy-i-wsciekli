f57a565960da561958fa79f27b4c2317d609c9aa
rafalkordas <34771872+rafalkordas@users.noreply.github.com>
2018-04-25 09:33:02 +0200

    Update FactRepository.java
    Delete import Java.Iterator

--- src/main/java/com/codecool/expertsystem/repositories/FactRepository.java
+++ src/main/java/com/codecool/expertsystem/repositories/FactRepository.java
@@ -4,7 +4,6 @@ import com.codecool.expertsystem.iterators.FactIterator;
 import com.codecool.expertsystem.questionnaire.Fact;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class FactRepository {
