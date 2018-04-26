a6abb03c163f6322dbfd3f0c9967daf24fb4c2cf
wluka85 <wluka85@gmail.com>
2018-04-26 12:19:14 +0200

    Hot fix in Answer class

--- src/main/java/com/codecool/expertsystem/questionnaire/Answer.java
+++ src/main/java/com/codecool/expertsystem/questionnaire/Answer.java
@@ -1,16 +1,18 @@
 package com.codecool.expertsystem.questionnaire;
 
 import com.codecool.expertsystem.questionnaire.value.Value;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Answer {
 
-    private List<Value> valuesList;
+    private List<Value> valuesList = new ArrayList<>();
 
     public boolean evaluateAnswerByInput(String input) {
         for (Value value : valuesList) {
             if (value.getInputPattern().contains(input)) {
-                return true;
+                return value.getSelectionType();
             }
         }
 
