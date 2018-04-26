eea53bb0f81a5f52db1cb284b6c2d11fe69f69c8
Maciej Sikora <m.j.p.sik@gmail.com>
2018-04-26 21:56:50 +0200

    Update App.java

--- src/main/java/com/codecool/expertsystem/App.java
+++ src/main/java/com/codecool/expertsystem/App.java
@@ -1,22 +1,32 @@
 package com.codecool.expertsystem;
 
+import com.codecool.expertsystem.view.Display;
 import com.codecool.expertsystem.parsers.FactParser;
 import com.codecool.expertsystem.parsers.RuleParser;
+import com.codecool.expertsystem.error.CarNotInXmlException;
 
-public class App
-{
+public class App {
+
     public static void main( String[] args ) {
-        FactParser factParser = new FactParser();
-        RuleParser ruleParser = new RuleParser();
+        String answer;
+        ESProvider esProvider = new ESProvider(new FactParser(), new RuleParser());
+        do {
 
-        ESProvider esProvider = new ESProvider(factParser, ruleParser);
+            esProvider.resetAnswers();
+            Display.clearScreen();
+            Display.printTitleMenu();
+            esProvider.collectAnswers();
 
-        Display.printTitleMenu();
+            try {
+                Display.printMessage("We found car/s for you:\n" + esProvider.evaluate());
+            } catch (CarNotInXmlException e) {
+                Display.printMessage("There is no car matching your requirements.");
+            }
+            finally{
+                answer = Display.getStringInput("do you want to answer questions again? (y/n)");
+            }
 
-        esProvider.collectAnswers();
-        try {
-            Display.printMessage("We found car/s for you:\n" + esProvider.evaluate());
-        } catch (Exception e) {
-            Display.printMessage("There is no car matching your requirements.");
-        }
-    }}
+        } while (answer.equals("y"));
+
+    }
+}
