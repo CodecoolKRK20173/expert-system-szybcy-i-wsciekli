9670a324a9b398c275f8bd2ee8dbc300916da7ed
wluka85 <wluka85@gmail.com>
2018-04-26 12:14:58 +0200

    Implement main method in App class

--- src/main/java/com/codecool/expertsystem/App.java
+++ src/main/java/com/codecool/expertsystem/App.java
@@ -1,13 +1,24 @@
 package com.codecool.expertsystem;
 
+import com.codecool.expertsystem.parsers.FactParser;
+import com.codecool.expertsystem.parsers.RuleParser;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+    public static void main( String[] args ) {
+        FactParser factParser = new FactParser();
+        RuleParser ruleParser = new RuleParser();
+
+        ESProvider esProvider = new ESProvider(factParser, ruleParser);
+
+        esProvider.collectAnswers();
+        try {
+            System.out.println(esProvider.evaluate());
+        } catch (Exception e) {
+            System.out.println("Cars out of bounds exceptionno");
+        }
+    }}
