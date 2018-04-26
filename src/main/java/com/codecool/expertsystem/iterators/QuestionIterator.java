141d84211ae1b1ff867c74a52ebc6b934fe891d0
wluka85 <wluka85@gmail.com>
2018-04-26 12:21:47 +0200

    Hotfix in QuestionIterator class

--- src/main/java/com/codecool/expertsystem/iterators/QuestionIterator.java
+++ src/main/java/com/codecool/expertsystem/iterators/QuestionIterator.java
@@ -3,6 +3,7 @@ package com.codecool.expertsystem.iterators;
 import com.codecool.expertsystem.questionnaire.Question;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class QuestionIterator implements Iterator {
@@ -10,7 +11,7 @@ public class QuestionIterator implements Iterator {
     private List<Question> questionList;
     private int index = 0;
 
-    public QuestionIterator(ArrayList<Question> questionList) {
+    public QuestionIterator(List<Question> questionList) {
         this.questionList = questionList;
 
     }
