bccb6822ab91567231214cd7ddf642358a6de705
wluka85 <wluka85@gmail.com>
2018-04-26 14:30:03 +0200

    Hotfix in Answer class

--- src/main/java/com/codecool/expertsystem/questionnaire/Answer.java
+++ src/main/java/com/codecool/expertsystem/questionnaire/Answer.java
@@ -3,6 +3,7 @@ package com.codecool.expertsystem.questionnaire;
 import com.codecool.expertsystem.questionnaire.value.Value;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Answer {
@@ -10,10 +11,15 @@ public class Answer {
     private List<Value> valuesList = new ArrayList<>();
 
     public boolean evaluateAnswerByInput(String input) {
+        List<String> values = new ArrayList<>();
+        values = Arrays.asList(input.split(","));
         for (Value value : valuesList) {
-            if (value.getInputPattern().contains(input)) {
-                return value.getSelectionType();
+            for (String param : values) {
+                if (value.getInputPattern().contains(param)) {
+                    return value.getSelectionType();
+                }
             }
+
         }
 
         return false;
