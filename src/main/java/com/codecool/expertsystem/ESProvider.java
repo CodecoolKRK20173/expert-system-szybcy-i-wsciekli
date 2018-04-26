d34cbc256154ea286bf076d8b41a99ad9c9baee2
wluka85 <wluka85@gmail.com>
2018-04-26 09:36:41 +0200

    Implement methods collectAnswer and getAnswerByQuestion in ESProvider class

--- src/main/java/com/codecool/expertsystem/ESProvider.java
+++ src/main/java/com/codecool/expertsystem/ESProvider.java
@@ -1,9 +1,16 @@
 package com.codecool.expertsystem;
 
+import com.codecool.expertsystem.iterators.QuestionIterator;
+import com.codecool.expertsystem.parsers.FactParser;
+import com.codecool.expertsystem.parsers.RuleParser;
 import com.codecool.expertsystem.questionnaire.Answer;
+import com.codecool.expertsystem.questionnaire.Question;
+import com.codecool.expertsystem.repositories.RuleRepository;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Class representing Expert System Provider.
@@ -12,32 +19,45 @@ public class ESProvider {
     private FactParser factParser;
     private RuleParser ruleParser;
     private RuleRepository ruleRepository;
+    private String input;
 
-    private List<Answer> answersList = new ArrayList();
+    private Map<String, Boolean> answersMap = new HashMap<>();
 
     public ESProvider(FactParser factParser, RuleParser ruleParser) {
 
         this.ruleParser = ruleParser;
         this.factParser = factParser;
+
         this.ruleRepository = ruleParser.getRuleRepository();
+
+
 
     }
 
 
 
     public void collectAnswers() {
+        Scanner userInput = new Scanner(System.in);
 
-        //TODO: implement
+        Iterator<Question> questionIterator = ruleRepository.getIterator();
+
+        while (questionIterator.hasNext()) {
+            Question nextQuestion = questionIterator.next();
+            System.out.println(nextQuestion.getQuestion());
+            String input = userInput.nextLine();
+            boolean evaluatedAnswer = nextQuestion.getEvaluatedAnswer(input);
+
+            answersMap.put(nextQuestion.getId(), evaluatedAnswer);
+        }
 
     }
 
     public boolean getAnswerByQuestion(String questionID) {
-
-        return false;    //TODO: implement
+        return answersMap.get(questionID);
 
     }
 
-    public String evaluate(){
+    public String evaluate() {
         return ""; //TODO: implement
     }
 
