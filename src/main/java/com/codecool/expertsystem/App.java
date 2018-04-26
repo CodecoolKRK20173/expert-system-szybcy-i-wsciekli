0b15882c74065a7edfd1644f6d42713105321d4b
kret0 <mjp.sik@gmail.com>
2018-04-26 21:45:40 +0200

    Remove invalid docstring (maven archetype artifact)

--- src/main/java/com/codecool/expertsystem/App.java
+++ src/main/java/com/codecool/expertsystem/App.java
@@ -1,36 +1,22 @@
 package com.codecool.expertsystem;
 
-import com.codecool.expertsystem.view.Display;
 import com.codecool.expertsystem.parsers.FactParser;
 import com.codecool.expertsystem.parsers.RuleParser;
-import com.codecool.expertsystem.error.CarNotInXmlException;
 
-/**
- * Hello world!
- *
- */
-public class App {
-
+public class App
+{
     public static void main( String[] args ) {
-        String answer;
-        ESProvider esProvider = new ESProvider(new FactParser(), new RuleParser());
-        do {
+        FactParser factParser = new FactParser();
+        RuleParser ruleParser = new RuleParser();
 
-            esProvider.resetAnswers();
-            Display.clearScreen();
-            Display.printTitleMenu();
-            esProvider.collectAnswers();
+        ESProvider esProvider = new ESProvider(factParser, ruleParser);
 
-            try {
-                Display.printMessage("We found car/s for you:\n" + esProvider.evaluate());
-            } catch (CarNotInXmlException e) {
-                Display.printMessage("There is no car matching your requirements.");
-            }
-            finally{
-                answer = Display.getStringInput("do you want to answer questions again? (y/n)");
-            }
+        Display.printTitleMenu();
 
-        } while (answer.equals("y"));
-
-    }
-}
+        esProvider.collectAnswers();
+        try {
+            Display.printMessage("We found car/s for you:\n" + esProvider.evaluate());
+        } catch (Exception e) {
+            Display.printMessage("There is no car matching your requirements.");
+        }
+    }}
